0bfc97457419b2d4bc1aa752a62a0b4ebf48bd88
“tomasdelgadin” <tdelgadin@itba.edu.ar>
2024-06-23 01:15:32 +0000

    auto commit 1       20240623 011530

--- src/lightgbm/454_lightgbm_final_libre.r
+++ src/lightgbm/454_lightgbm_final_libre.r
@@ -11,18 +11,18 @@ require("lightgbm")
 # defino los parametros de la corrida, en una lista, la variable global  PARAM
 #  muy pronto esto se leera desde un archivo formato .yaml
 PARAM <- list()
-PARAM$experimento <- "KA4540_LGBM"
+PARAM$experimento <- "KA4540"
 
 PARAM$input$dataset <- "~/datasets/dataset_pequeno.csv"
 PARAM$input$training <- c(202107) # meses donde se entrena el modelo
 PARAM$input$future <- c(202109) # meses donde se aplica el modelo
 
 
-PARAM$finalmodel$num_iterations <- 1036
-PARAM$finalmodel$learning_rate <- 0.0100597937033485
-PARAM$finalmodel$feature_fraction <- 0.649897468225194
-PARAM$finalmodel$min_data_in_leaf <- 414
-PARAM$finalmodel$num_leaves <- 839
+PARAM$finalmodel$num_iterations <- 559
+PARAM$finalmodel$learning_rate <- 0.0100746999
+PARAM$finalmodel$feature_fraction <- 0.5144127527
+PARAM$finalmodel$min_data_in_leaf <- 505
+PARAM$finalmodel$num_leaves <- 44
 
 
 PARAM$finalmodel$max_bin <- 31

e4af4cf1272de2dcb613091f2c292917a250fa11
“tomasdelgadin” <tdelgadin@itba.edu.ar>
2024-07-05 21:58:07 +0000

    auto commit 1       20240705 215806

--- src/workflows/617_workflow_base_f202107.r
+++ src/workflows/617_workflow_base_f202107.r
@@ -311,24 +311,24 @@ HT_tuning_base <- function( pinputexps, bypass=FALSE)
     force_row_wise = TRUE, # para reducir warnings
     verbosity = -100,
     max_bin = 31L, # lo debo dejar fijo, no participa de la BO
-    pos_bagging_fraction = 1.0, # 0.0 < pos_bagging_fraction <= 1.0
-    neg_bagging_fraction = 1.0, # 0.0 < neg_bagging_fraction <= 1.0
     is_unbalance = FALSE, #
     extra_trees = FALSE,
-   
-     # Parte variable
-    
-    max_depth = -1L, # -1 significa no limitar,  por ahora lo dejo fijo
-    min_gain_to_split = 0.0, # min_gain_to_split >= 0.0
-    min_sum_hessian_in_leaf = 0.001, #  min_sum_hessian_in_leaf >= 0.0
-    lambda_l1 = 0.0, # lambda_l1 >= 0.0
-    lambda_l2 = 0.0, # lambda_l2 >= 0.0
-    num_iterations = 9999, # un numero muy grande, lo limita early_stopping_rounds
-    bagging_fraction = 1.0, # 0.0 < bagging_fraction <= 1.0
     drop_rate = 0.1, # 0.0 < neg_bagging_fraction <= 1.0
     max_drop = 50, # <=0 means no limit
     skip_drop = 0.5, # 0.0 <= skip_drop <= 1.0
-    scale_pos_weight = 1.0, # scale_pos_weight > 0.0
+   
+     # Parte variable
+    
+    max_depth = c( 5L, 30L,  "integer" ), # -1 significa no limitar,  por ahora lo dejo fijo
+    min_gain_to_split = c( 0.0, 0.3 ), # min_gain_to_split >= 0.0
+    min_sum_hessian_in_leaf = c( 0.001, 0.1 ), #  min_sum_hessian_in_leaf >= 0.0
+    lambda_l1 = c( 0.0, 0.5 ), # lambda_l1 >= 0.0
+    lambda_l2 = c( 0.0, 0.5 ), # lambda_l2 >= 0.0
+    num_iterations = c( 1L, 1000L, "integer" ), # un numero muy grande, lo limita early_stopping_rounds
+    bagging_fraction = c( 0.5, 1.0 ), # 0.0 < bagging_fraction <= 1.0
+    pos_bagging_fraction = c( 0.5, 1.0 ), # 0.0 < pos_bagging_fraction <= 1.0
+    neg_bagging_fraction = c( 0.5, 1.0 ), # 0.0 < neg_bagging_fraction <= 1.0
+    scale_pos_weight = c( 1.0, 100.0 ), # scale_pos_weight > 0.0
     
     learning_rate = c( 0.02, 0.8 ),
     feature_fraction = c( 0.5, 0.9 ),
